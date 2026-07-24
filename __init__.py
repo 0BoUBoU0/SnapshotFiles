a2816fbca8efb3eeb73417117d82d1db3d04ff7e
Pullusb <bernou.samuel@gmail.com>
2026-07-24 15:32:46 +0200

    get_user function direct import

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ ADDON_VERSION = '. '.join([str(n) for n in bl_info["version"]])
 # import modules
 import bpy
 import os
-import getpass
+from getpass import getuser
 from socket import gethostname
 from shutil import copyfile
 from pathlib import Path
@@ -284,7 +284,7 @@ class FILE_OT_snapshotfiles(bpy.types.Operator):
                 user_comment = "None"
 
             bpy.data.texts[snap_text].cursor_set(3)
-            SnapHistoryText.write(f"Last snapshot made by: {getpass.getuser()} \n user comment: {user_comment} \n on: {gethostname()} ({platform}) \n Blender version: Blender {blender_version} \n the: {date_time} \n version based on: {bpy.context.preferences.addons[__name__].preferences.get_version_prop} \n >>> {snap_filepath}")
+            SnapHistoryText.write(f"Last snapshot made by: {getuser()} \n user comment: {user_comment} \n on: {gethostname()} ({platform}) \n Blender version: Blender {blender_version} \n the: {date_time} \n version based on: {bpy.context.preferences.addons[__name__].preferences.get_version_prop} \n >>> {snap_filepath}")
 
             ## create a fake file version file
             if user_fileversion_prop:
